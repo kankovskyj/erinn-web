f1a43acd9add9c06c2c3aad6a5bee1ffc614b2f6
Jan Kankovsky <52216257+kankovskyj@users.noreply.github.com>
2024-01-12 17:12:41 +0100

    bg remove

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex relative mb-24 lg:mb-0 flex-col w-11/12 mx-auto items-center mt-[8rem] lg:mt-24 place-content-center">
       <LocaleSwitcher />
 
-      <div className="mt-4 mb-4 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-100 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="mt-4 mb-4 relative flex place-items-center">
         <div className="relative w-[320px] h-[90px] lg:w-[460px] lg:h-[180px] dark:hidden block">
           <Image
             className="absolute  dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
